b10edf4c87ad28643dbedbcc5c7c6567980695fc
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-03-31 21:11:25 +0700

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
 
@@ -8,9 +9,9 @@ const router = require("./src/routes");
 
 require('./src/utils/MongoConnect');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use("/api/v1/", router);
 
 
