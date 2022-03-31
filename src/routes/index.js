5a3e337747e06a3ff8fb3e4aeef38c4f65360d09
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-03-31 21:18:21 +0700

    fix cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,16 @@ const router = express.Router()
 const User = require("../controller/User")
 const Match = require("../controller/Match")
 const Sport = require("../controller/Sport")
+const cors = require("cors")
+
+router
+    .use(cors({
+      origin: [
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    }))
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
